a16b46e122dd83b12a65b90c482bea2e7a784112
DELL <aziziahmed027@gmail.com>
2025-02-06 09:40:07 +0100

    Creating WeatherRepository.kt

---
+++ app/src/main/java/azizi/ahmed/weather/packages/repository/WeatherRepository.kt
@@ -0,0 +1,19 @@
+package azizi.ahmed.weather.packages.repository
+
+import azizi.ahmed.weather.packages.data.WeatherDataOrException
+import azizi.ahmed.weather.packages.model.Weather
+import azizi.ahmed.weather.packages.network.WeatherAPI
+import javax.inject.Inject
+
+class WeatherRepository @Inject constructor(private val weatherAPI: WeatherAPI) {
+    suspend fun getWeather(
+        cityQuery: String
+    ): WeatherDataOrException<Weather, Boolean, Exception> {
+        val response = try {
+            weatherAPI.getWeather(query = cityQuery)
+        } catch (exception: Exception) {
+            return WeatherDataOrException(exception = exception)
+        }
+        return WeatherDataOrException(data = response)
+    }
+}
