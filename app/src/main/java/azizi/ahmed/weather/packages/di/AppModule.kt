2772ee6223218434b84011776995f8c9e988fd98
DELL <aziziahmed027@gmail.com>
2025-02-06 09:37:12 +0100

    Creating AppModule.kt and providing weather API

---
+++ app/src/main/java/azizi/ahmed/weather/packages/di/AppModule.kt
@@ -0,0 +1,28 @@
+package azizi.ahmed.weather.packages.di
+
+import azizi.ahmed.weather.packages.network.WeatherAPI
+import azizi.ahmed.weather.packages.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideWeatherAPI(): WeatherAPI {
+        return Retrofit
+            .Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(WeatherAPI::class.java)
+    }
+}
