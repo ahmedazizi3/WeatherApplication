306db01f54e3fc20eaf962c2aced5480894304a6
DELL <aziziahmed027@gmail.com>
2025-02-06 09:41:19 +0100

    Reconstructing code

--- app/src/main/java/azizi/ahmed/weather/packages/screens/mainScreen/MainScreen.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/screens/mainScreen/MainScreen.kt
@@ -4,18 +4,33 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.produceState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import azizi.ahmed.weather.packages.data.WeatherDataOrException
+import azizi.ahmed.weather.packages.model.Weather
+import azizi.ahmed.weather.packages.viewModel.MainScreenViewModel
 
 @Composable
 fun MainScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    mainScreenViewModel: MainScreenViewModel = viewModel()
 ) {
+    val weatherData = produceState<WeatherDataOrException<Weather, Boolean, Exception>>(
+        initialValue = WeatherDataOrException(loading = true)
+    ) {
+        value = mainScreenViewModel.getWeatherData(
+            city = "Ouargla"
+        )
+    }.value
+
     Column(
         modifier = modifier
             .fillMaxSize()
@@ -23,11 +38,15 @@ fun MainScreen(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "Main Screen",
-            fontWeight = FontWeight.Bold,
-            fontSize = 30.sp,
-            color = Color.LightGray
-        )
+        if(weatherData.loading == true) {
+            CircularProgressIndicator()
+        } else if(weatherData.data != null) {
+            Text(
+                text = "Main Screen ${weatherData.data!!.city.country}",
+                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
+                color = Color.LightGray
+            )
+        }
     }
 }
