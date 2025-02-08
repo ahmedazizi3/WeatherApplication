f3d62b6cbe3447957cab72e0554a50500f830cd1
DELL <aziziahmed027@gmail.com>
2025-02-08 19:28:21 +0100

    Handling the pass of the name of the city from searchScreen to MainScreen

--- app/src/main/java/azizi/ahmed/weather/packages/navigation/weatherNavigation.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/navigation/weatherNavigation.kt
@@ -3,11 +3,13 @@ package azizi.ahmed.weather.packages.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import azizi.ahmed.weather.packages.screens.aboutScreen.AboutAppScreen
-import azizi.ahmed.weather.packages.screens.favoriteScreen.FavoriteScreen
+import azizi.ahmed.weather.packages.screens.favoriteScreen.FavoritesScreen
 import azizi.ahmed.weather.packages.screens.mainScreen.MainScreen
 import azizi.ahmed.weather.packages.screens.searchScreen.SearchScreen
 import azizi.ahmed.weather.packages.screens.settingsScreen.SettingsScreen
@@ -23,20 +25,44 @@ fun WeatherNavigation(
         navController = navController,
         startDestination = WeatherScreens.SplashScreen.route
     ) {
+        val defaultCity = "Ouargla"
         composable(
             route = WeatherScreens.SplashScreen.route
         ) {
             SplashScreen {
-                navController.navigate(WeatherScreens.MainScreen.route)
+                navController.navigate(WeatherScreens.MainScreen.route +"/$defaultCity")
             }
         }
 
+        val route = WeatherScreens.MainScreen.route
         composable(
-            route = WeatherScreens.MainScreen.route
-        ) {
-            MainScreen(
-                navController = navController
+            route = "$route/{city}",
+            arguments = listOf(
+                navArgument(
+                    name = "city"
+                ) {
+                    type = NavType.StringType
+                }
             )
+        ) { navBack ->
+            navBack.arguments?.getString("city").let { city ->
+                MainScreen(
+                    navController = navController,
+                    navigateToSearchScreen = {
+                        navController.navigate(WeatherScreens.SearchScreen.route)
+                    },
+                    city = city,
+                    navigateToFavoriteScreen = {
+                        navController.navigate(WeatherScreens.FavoriteScreen.route)
+                    },
+                    navigateToAboutScreen = {
+                        navController.navigate(WeatherScreens.AboutAppScreen.route)
+                    },
+                    navigateToSettingsScreen = {
+                        navController.navigate(WeatherScreens.SettingsScreen.route)
+                    }
+                )
+            }
         }
 
         composable(
@@ -50,7 +76,7 @@ fun WeatherNavigation(
         composable(
             route = WeatherScreens.FavoriteScreen.route
         ) {
-            FavoriteScreen(
+            FavoritesScreen(
                 navController = navController
             )
         }
@@ -59,7 +85,13 @@ fun WeatherNavigation(
             route = WeatherScreens.SearchScreen.route
         ) {
             SearchScreen(
-                navController = navController
+                navController = navController,
+                navigateBackToMainScreen = {
+                    navController.popBackStack()
+                },
+                navigateToMainScreen = { mCity ->
+                    navController.navigate(WeatherScreens.MainScreen.route +"/$mCity")
+                }
             )
         }
 
