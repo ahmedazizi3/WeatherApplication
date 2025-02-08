6e171e937e0d4f52f4988b3a60ca18c34460d09e
DELL <aziziahmed027@gmail.com>
2025-02-08 11:36:30 +0100

    Creating some formatters in Formatter.kt function

--- app/src/main/java/azizi/ahmed/weather/packages/utils/Formatter.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/utils/Formatter.kt
@@ -4,10 +4,32 @@ import java.time.Instant
 import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 
-fun formatter(timestamp: Long): String {
+fun dateFormatter(timestamp: Long): String {
     val formatter = DateTimeFormatter.ofPattern("EEEE, dd MMMM yyyy")
         .withZone(ZoneId.systemDefault())
 
     // Convert seconds to milliseconds
     return formatter.format(Instant.ofEpochSecond(timestamp))
 }
+
+fun timeFormatter(timestamp: Long): String {
+    val formatter = DateTimeFormatter.ofPattern("HH:mm")
+        .withZone(ZoneId.systemDefault())
+
+    return formatter.format(Instant.ofEpochSecond(timestamp))
+}
+
+fun dayNameFormatter(timestamp: Long): String {
+    val formatter = DateTimeFormatter.ofPattern("EE")
+        .withZone(ZoneId.systemDefault())
+
+    // Convert seconds to milliseconds
+    return formatter.format(Instant.ofEpochSecond(timestamp))
+}
+
+fun formatMilesToKm(miles: Double): String {
+    val km = miles * 1.60934
+    return String.format("%.2f", km)
+}
+
+
