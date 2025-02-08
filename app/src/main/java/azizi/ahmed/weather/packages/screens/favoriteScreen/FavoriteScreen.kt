c417f40aeb089632ae6e088f9e4b7cf6d988bf1c
DELL <aziziahmed027@gmail.com>
2025-02-08 19:23:31 +0100

    Creating the initial view of SettingsScreen.kt, AboutAppScreen.kt and FavoriteScreen.kt

--- app/src/main/java/azizi/ahmed/weather/packages/screens/favoriteScreen/FavoriteScreen.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/screens/favoriteScreen/FavoriteScreen.kt
@@ -1,13 +1,53 @@
 package azizi.ahmed.weather.packages.screens.favoriteScreen
 
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import azizi.ahmed.weather.packages.components.common.WeatherTopBar
 
 @Composable
-fun FavoriteScreen(
+fun FavoritesScreen(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
-
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize(),
+        containerColor = Color.White,
+        topBar = {
+            WeatherTopBar(
+                isMainScreen = false,
+                onIcon3Clicked = {
+                    navController.popBackStack()
+                },
+                title = "Favorites"
+            )
+        }
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "This is Favorites Screen",
+                color = Color.Black,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 }
--- app/src/main/java/azizi/ahmed/weather/packages/screens/settingsScreen/SettingsScreen.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/screens/settingsScreen/SettingsScreen.kt
@@ -1,13 +1,52 @@
 package azizi.ahmed.weather.packages.screens.settingsScreen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import azizi.ahmed.weather.packages.components.common.WeatherTopBar
 
 @Composable
 fun SettingsScreen(
     modifier: Modifier = Modifier,
     navController: NavController
 ) {
-
+    Scaffold(
+        modifier = modifier
+            .fillMaxSize(),
+        containerColor = Color.White,
+        topBar = {
+            WeatherTopBar(
+                isMainScreen = false,
+                onIcon3Clicked = {
+                    navController.popBackStack()
+                },
+                title = "Setting"
+            )
+        }
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(it),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                "This is Settings Screen",
+                color = Color.Black,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
 }
