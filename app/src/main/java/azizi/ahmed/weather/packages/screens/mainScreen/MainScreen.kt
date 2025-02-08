79dac44c6d6857f5aeb5b8956f5f5b5a0839f407
DELL <aziziahmed027@gmail.com>
2025-02-08 11:37:25 +0100

    Adding Components to main screen

--- app/src/main/java/azizi/ahmed/weather/packages/screens/mainScreen/MainScreen.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/screens/mainScreen/MainScreen.kt
@@ -5,14 +5,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -27,15 +29,15 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import azizi.ahmed.weather.packages.components.common.WeatherTopBar
+import azizi.ahmed.weather.packages.components.mainScreen.DayRow
 import azizi.ahmed.weather.packages.components.mainScreen.HumidityWindPressureRow
+import azizi.ahmed.weather.packages.components.mainScreen.SunSetAndSunRiseRow
 import azizi.ahmed.weather.packages.components.mainScreen.TopCircle
 import azizi.ahmed.weather.packages.data.WeatherDataOrException
 import azizi.ahmed.weather.packages.model.Weather
-import azizi.ahmed.weather.packages.utils.formatter
+import azizi.ahmed.weather.packages.model.WeatherItem
+import azizi.ahmed.weather.packages.utils.dateFormatter
 import azizi.ahmed.weather.packages.viewModel.MainScreenViewModel
-import java.time.LocalDate
-import java.time.format.DateTimeFormatter
-import java.util.Locale
 
 @Composable
 fun MainScreen(
@@ -99,25 +101,50 @@ fun MainScreen(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Top
             ) {
-//                Date of the day
                 Spacer(modifier = modifier.height(10.dp))
 
                 Text(
-                    text = formatter(weatherData.data!!.list[0].dt.toLong()) ,
+                    text = dateFormatter(weatherData.data!!.list[0].date.toLong()) ,
                     fontWeight = FontWeight.Normal,
                     fontSize = 20.sp,
                     color = Color.LightGray
                 )
 
-//                Adding the Top Circle
                 TopCircle(
                     data = weatherData.data!!
                 )
 
-                Spacer(modifier = modifier.height(10.dp))
-
                 HumidityWindPressureRow(weather = weatherData.data!!)
 
+                HorizontalDivider(
+                    color = Color.LightGray
+                )
+
+                SunSetAndSunRiseRow(
+                    weather = weatherData.data!!
+                )
+
+                Text(
+                    text = "7 Days Forecast",
+                    color = Color.LightGray,
+                    fontSize = 25.sp
+                )
+
+                Spacer(modifier = modifier.height(10.dp))
+
+                LazyColumn(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(4.dp)
+                ) {
+                    items(
+                        items = weatherData.data!!.list
+                    ) { item: WeatherItem ->
+                        DayRow(
+                            weather = item
+                        )
+                    }
+                }
             }
         }
     }
