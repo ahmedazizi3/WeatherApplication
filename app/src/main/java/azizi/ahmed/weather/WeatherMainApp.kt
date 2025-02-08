1220fd970ad8fb00e7af236d4bd0202404dcba60
DELL <aziziahmed027@gmail.com>
2025-02-08 09:37:56 +0100

    Creating WeatherMainApp.kt which will hold the running function

---
+++ app/src/main/java/azizi/ahmed/weather/WeatherMainApp.kt
@@ -0,0 +1,14 @@
+package azizi.ahmed.weather
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import azizi.ahmed.weather.packages.navigation.WeatherNavigation
+
+@Composable
+fun WeatherMainApp(
+    modifier: Modifier = Modifier
+) {
+    WeatherNavigation(
+        modifier = modifier
+    )
+}
