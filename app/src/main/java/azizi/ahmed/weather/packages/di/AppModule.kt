a9bfb41fc07c17f0a66cb6daab47dacc0523dece
DELL <aziziahmed027@gmail.com>
2025-02-10 12:59:05 +0100

    Adding WeatherDao to AppModule.kt

--- app/src/main/java/azizi/ahmed/weather/packages/di/AppModule.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/di/AppModule.kt
@@ -1,10 +1,16 @@
 package azizi.ahmed.weather.packages.di
 
+import android.app.Application
+import android.content.Context
+import androidx.room.Room
+import azizi.ahmed.weather.packages.data.WeatherDAO
+import azizi.ahmed.weather.packages.data.WeatherDatabase
 import azizi.ahmed.weather.packages.network.WeatherAPI
 import azizi.ahmed.weather.packages.utils.Constants
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -14,6 +20,22 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 class AppModule {
+
+    @Singleton
+    @Provides
+    fun provideWeatherDAO(weatherDatabase: WeatherDatabase): WeatherDAO = weatherDatabase.weatherDAO()
+
+    @Singleton
+    @Provides
+    fun provideAppDatabase(@ApplicationContext applicationContext: Context): WeatherDatabase
+    = Room
+        .databaseBuilder(
+            context = applicationContext,
+            klass = WeatherDatabase::class.java,
+            name = "weatherDatabase"
+        )
+        .fallbackToDestructiveMigration()
+        .build()
 
     @Provides
     @Singleton
