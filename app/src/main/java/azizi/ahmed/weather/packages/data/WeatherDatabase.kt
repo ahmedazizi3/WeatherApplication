a2c993a959f03b69f5b7d01f25fb0df6bcac03aa
DELL <aziziahmed027@gmail.com>
2025-02-12 09:50:01 +0100

    Introduce UnitEntity to WeatherDatabase and update version to 2.

--- app/src/main/java/azizi/ahmed/weather/packages/data/WeatherDatabase.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/data/WeatherDatabase.kt
@@ -3,8 +3,18 @@ package azizi.ahmed.weather.packages.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import azizi.ahmed.weather.packages.model.FavoriteEntity
+import azizi.ahmed.weather.packages.model.UnitEntity
 
-@Database(entities = [FavoriteEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [
+        FavoriteEntity::class,
+        UnitEntity::class
+    ],
+    version = 2,
+    exportSchema = false
+)
 abstract class WeatherDatabase: RoomDatabase() {
-    abstract fun weatherDAO(): WeatherDAO
+    abstract fun weatherDAO(): FavoriteDAO
+    abstract fun unitsDAO(): UnitDAO
+
 }
