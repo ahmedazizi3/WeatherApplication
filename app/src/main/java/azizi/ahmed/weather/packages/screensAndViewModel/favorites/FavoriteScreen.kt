61d26b3418eb040faa255b86d3a81bad4995bba0
DELL <aziziahmed027@gmail.com>
2025-02-12 09:46:59 +0100

    Changing names from weather to favorite and creating the LazyColumn inside FavoriteScreen.kt

--- app/src/main/java/azizi/ahmed/weather/packages/screensAndViewModel/favorites/FavoriteScreen.kt
+++ app/src/main/java/azizi/ahmed/weather/packages/screensAndViewModel/favorites/FavoriteScreen.kt
@@ -5,21 +5,24 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import azizi.ahmed.weather.packages.components.common.WeatherTopBar
+import azizi.ahmed.weather.packages.components.favoriteScreen.FavoriteCityRow
 
 @Composable
 fun FavoritesScreen(
     modifier: Modifier = Modifier,
-    navController: NavController
+    navController: NavController,
+    favoritesViewModel: FavoritesViewModel = hiltViewModel()
 ) {
     Scaffold(
         modifier = modifier
@@ -40,14 +43,18 @@ fun FavoritesScreen(
                 .fillMaxSize()
                 .padding(it),
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Top
         ) {
-            Text(
-                "This is Favorites Screen",
-                color = Color.Black,
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold
-            )
+            val favoritesList = favoritesViewModel.favoriteList.collectAsState().value
+
+            LazyColumn {
+                items(favoritesList) {
+                    FavoriteCityRow(
+                        favorite = it,
+                        navController = navController
+                    )
+                }
+            }
         }
     }
 }
